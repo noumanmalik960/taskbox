12f1ad48933241e378fdc109bd7b57f9378cefa5
Nouman Javed <noumanmalik960@gmail.com>
2021-01-07 21:25:23 +0500

    added all till `composite component`

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Task from "./components/Task";
-import TaskStories, { Default } from "./components/Task.stories";
 
 function App() {
   return <div className="App"></div>;
